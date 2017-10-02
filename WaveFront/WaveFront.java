2346750d052822ead213cb4f3edeb12df35c6324
Cesar <cecerhcp0@hotmail.com>
2017-10-02 17:35:53 -0300

    adicionei o caminho da volta do wavefront, agora no fim tem uma pilha com o caminho do inicio ateh a meta

--- WaveFront/WaveFront.java
+++ WaveFront/WaveFront.java
@@ -3,6 +3,7 @@ import java.util.Queue;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Stack;
 import java.awt.geom.Line2D;
 
 
@@ -31,6 +32,8 @@ public class WaveFront {
         Graph G = new Graph(1189, 841, dim);
         double startX = 113 / dim;
         double startY = 35 / dim;
+        int goalX = 90 / dim;
+        int goalY = 730 / dim;
 
         for (int y = 0; y < G.height; y++) {
             for (int x = 0; x < G.width; x++) {
@@ -56,22 +59,49 @@ public class WaveFront {
                 StdDraw.setPenColor(StdDraw.BLUE);
                 StdDraw.point(x, y);
                 q.add(neighbor);
-                if (x == 9 && y == 73) {
+                if (x == goalX && y == goalY) {
                     q.clear();
                     break;
                 }
             }
         }
+
         StdDraw.setPenColor(StdDraw.GREEN);
         StdDraw.point(9, 73);
 
-
-        // int k = 0;
-        // while (parent[k] != k) {
-        //     System.out.println(k);
-        //     k = parent[k];
-        // }
-        // System.out.println(k);
+        Vertex v = new Vertex(goalX, goalY);
+        Stack<Vertex> path = new Stack();
+        path.add(v);
+        int minNeighborDist = G.width * G.height;
+        while (minNeighborDist > 1) {
+            Vertex minNeighbor = new Vertex(0, 0);
+            List<Vertex> neighbors = v.getNeighbors(8, G.width, G.height);
+            for (Vertex neighbor : neighbors) {
+                int x = (int) neighbor.posX;
+                int y = (int) neighbor.posY;
+                if (G.map[y][x] == -1) continue;
+                if (G.map[y][x] == 0) {
+                    continue;
+                }
+                if (G.map[y][x] < minNeighborDist) {
+                    minNeighborDist = (int) G.map[y][x];
+                    minNeighbor = neighbor;
+                }
+            }
+            System.out.println(minNeighborDist);
+            v = minNeighbor;
+            path.push(minNeighbor);
+        }
+        path.push(new Vertex(startX, startY));
+        while (!path.isEmpty()) {
+            v = path.pop();
+            int vX = (int) v.posX;
+            int vY = (int) v.posY;
+            StdDraw.setPenColor(StdDraw.RED);
+            StdDraw.point(vX, vY);
+        }
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.point(startX, startY);
 
     }
 
